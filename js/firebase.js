15c282c479a3ca4d1adfef8f434a9b059ace472a
arpitmishr <74408351+arpitmishr@users.noreply.github.com>
2026-01-21 17:32:20 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,9 +1,7 @@
-// js/firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
 
-// Your provided configuration
 const firebaseConfig = {
   apiKey: "AIzaSyD08m5A0JA1VLkJkZ6Ktoz5dq6bVEgCLwA",
   authDomain: "sales-route-management.firebaseapp.com",
@@ -13,7 +11,6 @@ const firebaseConfig = {
   appId: "1:676952991059:web:910d0abca29c256636ada6"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
