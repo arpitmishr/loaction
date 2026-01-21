e307e12f38abd2a167f39cf1a127799780f14381
arpitmishr <74408351+arpitmishr@users.noreply.github.com>
2026-01-21 17:18:40 +0530

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,20 @@
+// js/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD08m5A0JA1VLkJkZ6Ktoz5dq6bVEgCLwA",
+  authDomain: "sales-route-management.firebaseapp.com",
+  projectId: "sales-route-management",
+  storageBucket: "sales-route-management.firebasestorage.app",
+  messagingSenderId: "676952991059",
+  appId: "1:676952991059:web:910d0abca29c256636ada6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
